0669cd8a145959cc21d37ea7de046dc7fd192422
alsvike <148956728+alsvike@users.noreply.github.com>
2024-11-10 14:00:25 +0100

    Add chapter 1 user choices to constants for tracking exploration options

--- constants.py
+++ constants.py
@@ -6,3 +6,41 @@ weapons = {
     "crossbow" : {"min_damage" : 18, "max_damage" : 25, "durability" : 5},    # High damage, very low durability
     "staff" : {"min_damage" : 5, "max_damage" : 9, "durability" : 18}         # Low damage, high durability
 }
+
+# Chapter 1 - Users Choices (boolean)
+chapter1_choices = {
+    "ancient_shrine" : False,
+    "abandoned_camp" : False,
+    "suspicious_mushroom_patch" : False,
+    "watchers_oak" : False,
+    "forgotten_tome" : False,
+    "echoing_cave" : False,
+    "shimmering_pond" : False,
+    "grave_of_ashes" : False,
+    "feral_woods" : False,
+    "runic_stones" : False,
+    "wailing_tree" : False,
+    "silver_thread_path" : False,
+    "frost_covered_altar" : False,
+    "cave_of_lost_shadows" : False,
+    "veil_of_light" : False,
+    "obsidian_shard" : False,
+    "whispering_whisp" : False,
+    "eerie_campfire" : False,
+    "rusted_weapon_rack" : False,
+    "howling_pit" : False,
+    "singing_stones" : False,
+    "cursed mirror" : False,
+    "twillight_lantern" : False,
+    "fallen_guardian" : False,
+    "spiders_web" : False,
+    "crystalline_shrine" : False,
+    "lost_travelers_skull" : False,
+    "mysterious_talisman" : False,
+    "ancient_skeleton" : False,
+    "veil_of_nightshade" : False,
+    "silent_tombstone" : False,
+    "wispkeepers_lantern" : False,
+    "enchanted_fountain" : False,
+    "broken_altar" : False,
+}
